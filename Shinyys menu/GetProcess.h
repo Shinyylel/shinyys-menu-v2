1a129aad388cad5624672406c03c955528337325
nick harrison <87631315+Shinyylel@users.noreply.github.com>
2021-09-22 14:41:24 -0500

    Add files via upload

---
+++ Shinyys menu/GetProcess.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <Windows.h>
+#include <TlHelp32.h>
+#include "Offsets.h"
+#include <stdlib.h>
+
+DWORD procId;
+HANDLE hProcess;
+
+uintptr_t GetModuleBaseAddress(const char* modName) {
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+	if (hSnap != INVALID_HANDLE_VALUE) {
+		MODULEENTRY32 modEntry;
+		modEntry.dwSize = sizeof(modEntry);
+		if (Module32First(hSnap, &modEntry)) {
+			do {
+				if (!strcmp(modEntry.szModule, modName)) {
+					CloseHandle(hSnap);
+					return (uintptr_t)modEntry.modBaseAddr;
+				}
+			} while (Module32Next(hSnap, &modEntry));
+		}
+	}
+}
+
+template<typename T> T RPM(SIZE_T address) {
+	T buffer;
+	ReadProcessMemory(hProcess, (LPCVOID)address, &buffer, sizeof(T), NULL);
+	return buffer;
+}
+
+template<typename T> void WPM(SIZE_T address, T buffer) {
+	WriteProcessMemory(hProcess, (LPVOID)address, &buffer, sizeof(buffer), NULL);
+}
+
+
+
