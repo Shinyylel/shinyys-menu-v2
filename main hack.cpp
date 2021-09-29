710bb6e9aa446a2abf8cad75952158797a1511c4
nick harrison <87631315+Shinyylel@users.noreply.github.com>
2021-09-28 23:51:47 -0500

    Update main hack.cpp

--- main hack.cpp
+++ main hack.cpp
@@ -5,7 +5,7 @@ int main() {
 
     hwnd = FindWindowA(NULL, "Counter-Strike: Global Offensive");
     GetWindowThreadProcessId(hwnd, &procId);
-    moduleBase = GetModuleBaseAddress("client.dll");
+    moduleBase = GetModuleBaseAddress(procId, L"client.dll");
     hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
 
     if (val.localPlayer == NULL)
@@ -33,77 +33,77 @@ int main() {
             Sleep(2000);
             exit(1);
         }
-    }     
+    }
 
+    Status = "0";
+    Status2 = "0";
+    Status3 = "0";
+    Status4 = "0";
+    Status6 = "0";
+
+    std::thread t1(on);
+
+    while (true)
+    {
+        if (GetKeyState(VK_F1) & 1)
+        {
+            enmteam();
+            Status = "1";
+            Sleep(1);
+        }
+        else
+        {
             Status = "0";
-            Status2 = "0";
-            Status3 = "0";
-            Status4 = "0";
+        }
+        if (GetKeyState(VK_F2) & 1)
+        {
+            team();
+            Status6 = "1";
+            Sleep(1);
+        }
+        else
+        {
             Status6 = "0";
-
-            std::thread t1(on);
-
-            while (true)
-            {
-                if (GetKeyState(VK_F1) & 1)
-                {
-                    enmteam();
-                    Status = "1";
-                    Sleep(1);
-                }
-                else
-                {
-                    Status = "0";
-                }
-                if (GetKeyState(VK_F2) & 1)
-                {
-                    team();
-                    Status6 = "1";
-                    Sleep(1);
-                }
-                else
-                {
-                    Status6 = "0";
-                }
-                if (GetKeyState(VK_F3) & 1)
-                {
-                    bhop();
-                    Status2 = "1";
-                    Sleep(1);
-                }
-                else
-                {
-                    Status2 = "0";
-                }
-                if (GetKeyState(VK_F4) & 1)
-                {
-                    triggerbot();
-                    Status3 = "1";
-                    Sleep(1);
-                }
-                else
-                {
-                    Status3 = "0";
-                }
-                if (GetKeyState(VK_F5) & 1)
-                {
-                    flash();
-                    Status4 = "1";
-                    Sleep(1);
-                }
-                else
-                {
-                    Status4 = "0";
-                }
-                if (GetKeyState(VK_F6) & 1)
-                {
-                    shoot();
-                    Status5 = "1";
-                    Sleep(1);
-                }
-                else
-                {
-                    Status5 = "0";
-                }
-            }   
+        }
+        if (GetKeyState(VK_F3) & 1)
+        {
+            bhop();
+            Status2 = "1";
+            Sleep(1);
+        }
+        else
+        {
+            Status2 = "0";
+        }
+        if (GetKeyState(VK_F4) & 1)
+        {
+            triggerbot();
+            Status3 = "1";
+            Sleep(1);
+        }
+        else
+        {
+            Status3 = "0";
+        }
+        if (GetKeyState(VK_F5) & 1)
+        {
+            flash();
+            Status4 = "1";
+            Sleep(1);
+        }
+        else
+        {
+            Status4 = "0";
+        }
+        if (GetKeyState(VK_F6) & 1)
+        {
+            shoot();
+            Status5 = "1";
+            Sleep(1);
+        }
+        else
+        {
+            Status5 = "0";
+        }
+    }
 }
