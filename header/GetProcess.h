fb28cc32a04f21ba5279e5af37d61bddc005a865
nick harrison <87631315+Shinyylel@users.noreply.github.com>
2021-09-28 23:53:01 -0500

    Update GetProcess.h

--- header/GetProcess.h
+++ header/GetProcess.h
@@ -7,20 +7,31 @@
 DWORD procId;
 HANDLE hProcess;
 
-uintptr_t GetModuleBaseAddress(const char* modName) {
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName) {
+	uintptr_t modBaseAddr = 0;
 	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
-	if (hSnap != INVALID_HANDLE_VALUE) {
+	if (hSnap != INVALID_HANDLE_VALUE)
+	{
 		MODULEENTRY32 modEntry;
 		modEntry.dwSize = sizeof(modEntry);
-		if (Module32First(hSnap, &modEntry)) {
-			do {
-				if (!strcmp(modEntry.szModule, modName)) {
-					CloseHandle(hSnap);
-					return (uintptr_t)modEntry.modBaseAddr;
+		if (Module32First(hSnap, &modEntry))
+		{
+			do
+			{
+				//same thing that GetProcId but for module
+				if (!_wcsicmp(modEntry.szModule, modName))
+				{
+					modBaseAddr = (uintptr_t)modEntry.modBaseAddr;
+					break;
 				}
+
+
 			} while (Module32Next(hSnap, &modEntry));
 		}
 	}
+	//close handle and return moduleBaseAddress
+	CloseHandle(hSnap);
+	return modBaseAddr;
 }
 
 template<typename T> T RPM(SIZE_T address) {
@@ -32,6 +43,3 @@ template<typename T> T RPM(SIZE_T address) {
 template<typename T> void WPM(SIZE_T address, T buffer) {
 	WriteProcessMemory(hProcess, (LPVOID)address, &buffer, sizeof(buffer), NULL);
 }
-
-
-
